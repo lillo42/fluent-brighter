416485b03b688236f48a88ebad1d0f5a0f7c5cfa
Rafael Lillo <7280959+lillo42@users.noreply.github.com>
2026-03-04 18:51:15 +0000

    fix build

--- src/Fluent.Brighter.GoogleCloud/Extensions/ConsumerBuilderExtensions.cs
+++ src/Fluent.Brighter.GoogleCloud/Extensions/ConsumerBuilderExtensions.cs
@@ -1,13 +1,10 @@
 using System;
-
 using Fluent.Brighter.GoogleCloud;
-
 using Paramore.Brighter;
 using Paramore.Brighter.Firestore;
 using Paramore.Brighter.Inbox.Firestore;
 using Paramore.Brighter.Inbox.Spanner;
 using Paramore.Brighter.MessagingGateway.GcpPubSub;
-
 using SpannerInboxBuilder = Fluent.Brighter.GoogleCloud.SpannerInboxBuilder;
 
 namespace Fluent.Brighter;
@@ -23,25 +20,30 @@ public static class ConsumerBuilderExtensions
     /// <param name="builder">The consumer builder instance.</param>
     /// <param name="subscription">The configured <see cref="GcpPubSubSubscription"/> to add.</param>
     /// <returns>The consumer builder for method chaining.</returns>
-    public static ConsumerBuilder AddPubSubSubscription(this ConsumerBuilder builder, GcpPubSubSubscription subscription)
+    public static ConsumerBuilder AddPubSubSubscription(
+        this ConsumerBuilder builder,
+        GcpPubSubSubscription subscription
+    )
     {
         return builder.AddSubscription(subscription);
     }
-    
+
     /// <summary>
     /// Adds a Google Cloud Pub/Sub subscription to the consumer builder using a configuration action.
     /// </summary>
     /// <param name="builder">The consumer builder instance.</param>
     /// <param name="configure">An action to configure the <see cref="GcpPubSubSubscriptionBuilder"/>.</param>
     /// <returns>The consumer builder for method chaining.</returns>
-    public static ConsumerBuilder AddPubSubSubscription(this ConsumerBuilder builder,
-        Action<GcpPubSubSubscriptionBuilder> configure)
+    public static ConsumerBuilder AddPubSubSubscription(
+        this ConsumerBuilder builder,
+        Action<GcpPubSubSubscriptionBuilder> configure
+    )
     {
         var subscription = new GcpPubSubSubscriptionBuilder();
         configure(subscription);
         return builder.AddSubscription(subscription.Build());
     }
-    
+
     /// <summary>
     /// Adds a Google Cloud Pub/Sub subscription to the consumer builder with a strongly-typed request using a configuration action.
     /// </summary>
@@ -49,7 +51,10 @@ public static class ConsumerBuilderExtensions
     /// <param name="builder">The consumer builder instance.</param>
     /// <param name="configure">An action to configure the <see cref="GcpPubSubSubscriptionBuilder"/>.</param>
     /// <returns>The consumer builder for method chaining.</returns>
-    public static ConsumerBuilder AddPubSubSubscription<TRequest>(this ConsumerBuilder builder, Action<GcpPubSubSubscriptionBuilder> configure) 
+    public static ConsumerBuilder AddPubSubSubscription<TRequest>(
+        this ConsumerBuilder builder,
+        Action<GcpPubSubSubscriptionBuilder> configure
+    )
         where TRequest : class, IRequest
     {
         var subscription = new GcpPubSubSubscriptionBuilder();
@@ -57,14 +62,17 @@ public static class ConsumerBuilderExtensions
         configure(subscription);
         return builder.AddSubscription(subscription.Build());
     }
-    
+
     /// <summary>
     /// Adds a Google Cloud Pub/Sub channel factory to the consumer builder.
     /// </summary>
     /// <param name="builder">The consumer builder instance.</param>
     /// <param name="configure">An action to configure the <see cref="GcpMessagingGatewayConnectionBuilder"/>.</param>
     /// <returns>The consumer builder for method chaining.</returns>
-    public static ConsumerBuilder AddPubSubChannelFactory(this ConsumerBuilder builder, Action<GcpMessagingGatewayConnectionBuilder> configure)
+    public static ConsumerBuilder AddPubSubChannelFactory(
+        this ConsumerBuilder builder,
+        Action<GcpMessagingGatewayConnectionBuilder> configure
+    )
     {
         var subscription = new GcpMessagingGatewayConnectionBuilder();
         configure(subscription);
@@ -77,75 +85,91 @@ public static class ConsumerBuilderExtensions
     /// <param name="builder">The consumer builder instance.</param>
     /// <param name="configure">An action to configure the <see cref="FirestoreInboxBuilder"/>.</param>
     /// <returns>The consumer builder for method chaining.</returns>
-    public static ConsumerBuilder UseFirestoreInbox(this ConsumerBuilder builder,
-        Action<FirestoreInboxBuilder> configure)
+    public static ConsumerBuilder UseFirestoreInbox(
+        this ConsumerBuilder builder,
+        Action<FirestoreInboxBuilder> configure
+    )
     {
         var configuration = new FirestoreInboxBuilder();
         configure(configuration);
         return builder.UseFirestoreInbox(configuration.Build());
     }
-    
+
     /// <summary>
     /// Configures the consumer to use Google Cloud Firestore as the inbox storage using an existing gateway connection.
     /// </summary>
     /// <param name="builder">The consumer builder instance.</param>
     /// <param name="connection">The <see cref="GcpMessagingGatewayConnection"/> containing credentials and project information.</param>
     /// <returns>The consumer builder for method chaining.</returns>
-    public static ConsumerBuilder UseFirestoreInbox(this ConsumerBuilder builder, GcpMessagingGatewayConnection connection)
+    public static ConsumerBuilder UseFirestoreInbox(
+        this ConsumerBuilder builder,
+        GcpMessagingGatewayConnection connection
+    )
     {
-        return builder.UseFirestoreInbox(cfg => cfg
-            .SetConfiguration(c => c
-                .SetCredential(connection.Credential)
-                .SetProjectId(connection.ProjectId)
-                .SetInbox(new FirestoreCollection{ Name = "inbox"})));
+        return builder.UseFirestoreInbox(cfg =>
+            cfg.SetConfiguration(c =>
+                c.SetCredential(connection.Credential)
+                    .SetProjectId(connection.ProjectId)
+                    .SetInbox(new FirestoreCollection { Name = "inbox" })
+            )
+        );
     }
-    
+
     /// <summary>
     /// Configures the consumer to use Google Cloud Firestore as the inbox storage with a pre-configured inbox instance.
     /// </summary>
     /// <param name="builder">The consumer builder instance.</param>
     /// <param name="inbox">The configured <see cref="FirestoreInbox"/> instance.</param>
     /// <returns>The consumer builder for method chaining.</returns>
-    public static ConsumerBuilder UseFirestoreInbox(this ConsumerBuilder builder, FirestoreInbox inbox)
+    public static ConsumerBuilder UseFirestoreInbox(
+        this ConsumerBuilder builder,
+        FirestoreInbox inbox
+    )
     {
         return builder.SetInbox(cfg => cfg.SetInbox(inbox));
     }
-    
+
     /// <summary>
     /// Configures the consumer to use Google Cloud Spanner as the inbox storage.
     /// </summary>
     /// <param name="builder">The consumer builder instance.</param>
     /// <param name="configure">An action to configure the <see cref="SpannerInboxBuilder"/>.</param>
     /// <returns>The consumer builder for method chaining.</returns>
-    public static ConsumerBuilder UseSpannerInbox(this ConsumerBuilder builder,
-        Action<SpannerInboxBuilder> configure)
+    public static ConsumerBuilder UseSpannerInbox(
+        this ConsumerBuilder builder,
+        Action<SpannerInboxBuilder> configure
+    )
     {
         var configuration = new SpannerInboxBuilder();
         configure(configuration);
         return builder.UseSpannerInbox(configuration.Build());
     }
-    
+
     /// <summary>
     /// Configures the consumer to use Google Cloud Spanner as the inbox storage with a connection string.
     /// </summary>
     /// <param name="builder">The consumer builder instance.</param>
     /// <param name="connectionString">The Spanner connection string.</param>
     /// <returns>The consumer builder for method chaining.</returns>
-    public static ConsumerBuilder UseSpannerInbox(this ConsumerBuilder builder, string connectionString)
+    public static ConsumerBuilder UseSpannerInbox(
+        this ConsumerBuilder builder,
+        string connectionString
+    )
     {
-        return builder.UseSpannerInbox(cfg => cfg
-            .SetConfiguration(c => c
-                .SetConnectionString(connectionString)
-                .SetInboxTableName("inbox")));
+        return builder.UseSpannerInbox(cfg =>
+            cfg.SetConfiguration(c =>
+                c.SetConnectionString(connectionString).SetInboxTableName("inbox")
+            )
+        );
     }
-    
+
     /// <summary>
     /// Configures the consumer to use Google Cloud Spanner as the inbox storage with a pre-configured inbox instance.
     /// </summary>
     /// <param name="builder">The consumer builder instance.</param>
-    /// <param name="inbox">The configured <see cref="SpannerInboxAsync"/> instance.</param>
+    /// <param name="inbox">The configured <see cref="SpannerInbox"/> instance.</param>
     /// <returns>The consumer builder for method chaining.</returns>
-    public static ConsumerBuilder UseSpannerInbox(this ConsumerBuilder builder, SpannerInboxAsync inbox)
+    public static ConsumerBuilder UseSpannerInbox(this ConsumerBuilder builder, SpannerInbox inbox)
     {
         return builder.SetInbox(cfg => cfg.SetInbox(inbox));
     }
--- src/Fluent.Brighter.GoogleCloud/SpannerInboxBuilder.cs
+++ src/Fluent.Brighter.GoogleCloud/SpannerInboxBuilder.cs
@@ -4,7 +4,7 @@ using Paramore.Brighter.Inbox.Spanner;
 namespace Fluent.Brighter.GoogleCloud;
 
 /// <summary>
-/// Builder class for creating instances of <see cref="SpannerInboxAsync"/>.
+/// Builder class for creating instances of <see cref="SpannerInbox"/>.
 /// Provides a fluent API for configuring Google Cloud Spanner inbox storage.
 /// </summary>
 public sealed class SpannerInboxBuilder
@@ -29,25 +29,28 @@ public sealed class SpannerInboxBuilder
     /// </summary>
     /// <param name="connectionProvider">The connection provider to use for managing database connections.</param>
     /// <returns>The builder instance for method chaining.</returns>
-    public SpannerInboxBuilder SetConnectionProvider(IAmARelationalDbConnectionProvider connectionProvider)
+    public SpannerInboxBuilder SetConnectionProvider(
+        IAmARelationalDbConnectionProvider connectionProvider
+    )
     {
         _connectionProvider = connectionProvider;
         return this;
     }
-    
+
     /// <summary>
-    /// Builds a new instance of <see cref="SpannerInboxAsync"/> using the configured settings.
+    /// Builds a new instance of <see cref="SpannerInbox"/> using the configured settings.
     /// </summary>
-    /// <returns>A configured <see cref="SpannerInboxAsync"/> instance.</returns>
+    /// <returns>A configured <see cref="SpannerInbox"/> instance.</returns>
     /// <exception cref="ConfigurationException">Thrown when configuration is not set.</exception>
-    internal SpannerInboxAsync Build()
+    internal SpannerInbox Build()
     {
         if (_configuration == null)
         {
             throw new ConfigurationException("Configuration is null");
         }
-        
-        return _connectionProvider == null ? new SpannerInboxAsync(_configuration) 
-            : new SpannerInboxAsync(_configuration, _connectionProvider);
+
+        return _connectionProvider == null
+            ? new SpannerInbox(_configuration)
+            : new SpannerInbox(_configuration, _connectionProvider);
     }
 }
